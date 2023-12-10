9f963a5a24257e5a957f6d9154629d05fffe8b52
Gökhan MERDEN <gokhan18m@yahoo.com>
2023-12-10 23:20:47 +0300

    Data projesinde güncelleme

--- data/repositories/Repository.cs
+++ data/repositories/Repository.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using data.interfaces;
+using Microsoft.EntityFrameworkCore;
 
 namespace data.repositories
 {
