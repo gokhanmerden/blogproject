ed6077fca0cdb657a17a27db113a6c8cd7160650
Gökhan MERDEN <gokhan18m@yahoo.com>
2023-12-10 23:27:44 +0300

    Property eklemesi yapıldı.

--- data/uows/BlogUnitOfWork.cs
+++ data/uows/BlogUnitOfWork.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using data.repositories;
+using data.tables;
 namespace data.uows
 {
     public class BlogUnitOfWork
@@ -10,6 +12,24 @@ namespace data.uows
 
         public BlogUnitOfWork() {
             _context = new BlogContext();
+        }
+        private Repository<Blog> _blogRepository;
+        private Repository<Post> _postRepository;
+        public Repository<Blog> blogRepository {
+            get {
+                if (_blogRepository == null) {
+                    _blogRepository = new Repository<Blog>(_context);
+                }
+                return _blogRepository;
+            }
+        }
+        public Repository<Post> postRepository {
+            get {
+                if (_postRepository == null) {
+                    _postRepository = new Repository<Post>(_context);
+                }
+                return _postRepository;
+            }
         }
     }
 }
